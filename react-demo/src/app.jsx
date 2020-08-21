93785f0c3c3e3dfae5878b9db8325c9bee932709
changxianglin <281532612@qq.com>
2020-08-22 02:41:35 +0800

    compiler react use webpack and webpack plugin and loader

---
+++ react-demo/src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>React 大法好</h1>
+    </div>
+  )
+}
+
+export default App 
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ react-demo/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  resolve: {
+    extensions: ['.wasm', '.mjs', '.js', '.jsx', '.json'],
+  },
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [
+              require.resolve('@babel/preset-react'),
+              [require.resolve('@babel/preset-env', {modules: false})]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    hot: true,
+  }
+}
